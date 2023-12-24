b56537d9bf8e38b5634231e0eea5b8a0016ac853
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-24 20:51:54 +0530

    Variables in GO

--- main.go
+++ main.go
@@ -3,5 +3,12 @@ package main
 import "fmt"
 
 func main() {
-	fmt.Println("Hello, World!")
+	var conferenceName = "Go Conference"
+	const tickets = 50
+	var remainingTickets = 50
+	fmt.Printf("Welcome to %v Booking System \n", conferenceName)
+	fmt.Println("Total tickets:", tickets)
+	fmt.Println("Available tickets:", remainingTickets)
+	fmt.Println("Get your ticket now!")
+
 }
