dc28af25002a725cccf279f2c1e45beebcb05b0b
Rishi-Mishra0704 <rishimishra0404@gmail.com>
2023-12-24 20:55:29 +0530

    some change

--- main.go
+++ main.go
@@ -7,8 +7,8 @@ func main() {
 	const tickets = 50
 	var remainingTickets = 50
 	fmt.Printf("Welcome to %v Booking System \n", conferenceName)
-	fmt.Println("Total tickets:", tickets)
-	fmt.Println("Available tickets:", remainingTickets)
+	fmt.Printf("Total tickets: %v \n", tickets)
+	fmt.Printf("Available tickets: %v \n", remainingTickets)
 	fmt.Println("Get your ticket now!")
 
 }
